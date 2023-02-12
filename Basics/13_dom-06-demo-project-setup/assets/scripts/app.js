ed7ca2aecc998d372040bf9322cb835cd519df42
melodiaz23 <meli.diaz23@gmail.com>
2023-02-12 18:58:38 -0500

    DOM project: rendering movie items

--- Basics/13_dom-06-demo-project-setup/assets/scripts/app.js
+++ Basics/13_dom-06-demo-project-setup/assets/scripts/app.js
@@ -7,9 +7,39 @@ const addButton = modal.querySelector(".btn--success"); // another approach
 const userInputs = modal.querySelectorAll("input");
 // CONTROLLING THE BACKDROP
 const backdrop = document.getElementById("backdrop");
+const entryText = document.getElementById('entry-text');
 const movies = [];
 
+const clearMovieInput = () => {
+  for (const usrInput of userInputs){
+    usrInput.value = '';
+  }
+}
 
+const updateUI = () => {
+  if (movies.length === 0){
+    entryText.style.display = 'block';
+  } else {
+    entryText.style.display = 'none';
+  }
+};
+
+const addNewMovie = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+   //backticks 'option' + '}'
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class ="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+  const ulMovieList = document.getElementById('movie-list');
+  ulMovieList.append(newMovieElement);
+}
 
 const addMovieHandler = () => {
   const tittleValue = userInputs[0].value; //.value because we are gonna have a value property there.
@@ -27,17 +57,19 @@ const addMovieHandler = () => {
     alert('Please enter a valid values (rating between 1 and 5).')
     return;
   }
-
   const newMovie = {
     title: tittleValue,
     image: imageUrlValue,
     rating: ratingValue
   };
-
   movies.push(newMovie);
   console.log(movies)
   toogleModal();
+  addNewMovie(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
+
+
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -46,6 +78,7 @@ const toggleBackdrop = () => {
 const toogleModal = () => {
   modal.classList.toggle('visible');
   toggleBackdrop();
+  clearMovieInput();
 }
 
 
