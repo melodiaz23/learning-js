488a593cd1c56b6794aaff3bf1ebd95e83b78fa5
melodiaz23 <meli.diaz23@gmail.com>
2023-03-06 23:26:29 -0500

    forEach method

--- Basics/14_arrays-01-starting-code/app.js
+++ Basics/14_arrays-01-starting-code/app.js
@@ -51,34 +51,55 @@
 // console.log(hobbies);
 // // If we put a negative index (-1, -2...) it looks to the end of the  array and look from the right. If we put -1 it will delete the last item.
 
-//OTHER METHODS
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10]
+// //OTHER METHODS
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10]
 
-console.log(testResults.slice()); // It return a brand new array, so is a way of copie an array.
+// console.log(testResults.slice()); // It return a brand new array, so is a way of copie an array.
 
-//Other use of array... 
-// It help us to selecting ranges of an array.
-const storedResults = testResults.slice(0, 2) // With this we specify a start and an end number. This give us an array. In this case: 0 - Starting index and 2 - final index without including the index 2
-const negativIndex = testResults.slice(-3, -1) // It also works. the starting point will be the -3 item of the array right to left and the last item will be the -1 (not imcluded). 
-const oneIndex = testResults.slice(2) // Select elements on the array, since the number of index specify and select every element up to the end.
+// //Other use of array... 
+// // It help us to selecting ranges of an array.
+// const storedResults = testResults.slice(0, 2) // With this we specify a start and an end number. This give us an array. In this case: 0 - Starting index and 2 - final index without including the index 2
+// const negativIndex = testResults.slice(-3, -1) // It also works. the starting point will be the -3 item of the array right to left and the last item will be the -1 (not imcluded). 
+// const oneIndex = testResults.slice(2) // Select elements on the array, since the number of index specify and select every element up to the end.
 
-// Add elements to an array and return a new array -> concat()
-const storedResults2 = testResults.concat([3.99, 2]); // Return a new array combining an existing array
-testResults.push(5.91); // Push add a new element to the array. 
+// // Add elements to an array and return a new array -> concat()
+// const storedResults2 = testResults.concat([3.99, 2]); // Return a new array combining an existing array
+// testResults.push(5.91); // Push add a new element to the array. 
 
-//To look for the index of one element in the array. .indexOf()
-console.log(testResults.indexOf(1.5)) // It returns the first index of the value we are passing. If we add a second argument, this will be the index will be looking for. 
-console.log(testResults.lastIndexOf(1.5)); // This start looking for the value, right to left.
-// It only works with primitive values, no with reference values. In that case, we have other method:
+// //To look for the index of one element in the array. .indexOf()
+// console.log(testResults.indexOf(1.5)) // It returns the first index of the value we are passing. If we add a second argument, this will be the index will be looking for. 
+// console.log(testResults.lastIndexOf(1.5)); // This start looking for the value, right to left.
+// // It only works with primitive values, no with reference values. In that case, we have other method:
 
-const personData = [{name: 'Max'}, {name: 'Manuel'}];
+// const personData = [{name: 'Max'}, {name: 'Manuel'}];
 
-const manuel = personData.find((person, idx, persons) => { // It return the same object are in the array. Find does not create a copy. It works with the reference value. This means that if we manipulate 'manuel' in this case, this gonna change on personData too.
-  return person.name === 'Manuel'; // It gonna return the first match.
-}); 
+// const manuel = personData.find((person, idx, persons) => { // It returns the same object that is in the array. Find does not create a copy. It works with the reference value. This means that if we manipulate 'manuel' in this case, this gonna change on personData too.
+//   return person.name === 'Manuel'; // It gonna return the first match.
+// }); 
 
-const maxIndex = personData.findIndex((person, idx, persons) => {
-  return person.name === 'Max'; // This gonna return the index.
+// const maxIndex = personData.findIndex((person, idx, persons) => {
+//   return person.name === 'Max'; // This gonna return the index.
+// });
+
+// // Also we have: 
+// console.log(testResults.includes(10.99)); // For primitive values. This will return true or false is it is on the array or not.
+
+// Other method...
+
+const prices = [10.99, 5.99, 3.99, 6.59]
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// Insted this: 
+// for (const price of prices){
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
+// With this method we can't now the index, we need to do it manually. 
+// We also can use forEach method
+prices.forEach((price, idx, prices) => {
+  const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)}
+  taxAdjustedPrices.push(priceObj);
 });
+console.log(taxAdjustedPrices); 
 
 
