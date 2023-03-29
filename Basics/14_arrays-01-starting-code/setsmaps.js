7178d01b1000f75af3d0c9d810d7fb5602c654c1
melodiaz23 <meli.diaz23@gmail.com>
2023-03-28 20:36:57 -0500

    WeakSet and WeakMap

--- Basics/14_arrays-01-starting-code/setsmaps.js
+++ Basics/14_arrays-01-starting-code/setsmaps.js
@@ -12,25 +12,44 @@
 // }
 // //if we want to go through all elements in a set. This return an iterable.
 
-// MAP
+// // MAP
 
-const person1 = {name: 'Max'};
-const person2 = {name: 'Manuel'};
+// const person1 = {name: 'Max'};
+// const person2 = {name: 'Manuel'};
 
-const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
-// const personData = new Map([[100, person1]]);
-personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+// const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+// // const personData = new Map([[100, person1]]);
+// personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+// console.log(personData);
+// console.log(personData.get(person1));
+
+// for (const [key, value] of personData.entries()){ // This give us key and value in one array. 
+//   console.log(key, value);
+// }
+
+// for (const key of personData.keys()){ // Give us all the key values.
+//   console.log(key);
+// }
+
+// for (const value of personData.values()){ // Give us all the values.
+//   console.log(value);
+// }
+
+// WEAKSET AND WEAK MAP
+let person = {name: 'Max'};
+const persons = new WeakSet(); // WeakSet allow us to store objects in a collection.
+persons.add(person); 
+
+// person = null;
+
+// console.log(person); // It will be equal to null
+console.log(persons); // It will be the object. 
+
+// WEAK MAP
+const personData = new WeakMap();
+personData.set(person, 'Extra info!');
+
+person = null;
+console.log(person);
 console.log(personData);
-console.log(personData.get(person1));
 
-for (const [key, value] of personData.entries()){ // This give us key and value in one array. 
-  console.log(key, value);
-}
-
-for (const key of personData.keys()){ // Give us all the key values.
-  console.log(key);
-}
-
-for (const key of personData.values()){ // Give us all the values.
-  console.log(key);
-}
