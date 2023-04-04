8761984ccfdfb913df1a22cbc10d80659aa73a82
melodiaz23 <meli.diaz23@gmail.com>
2023-04-04 07:28:49 -0500

    objects js filter added

--- Basics/16-objects-01-starting-project/assets/scripts/objects.js
+++ Basics/16-objects-01-starting-project/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
   
   if (movies.length === 0){
@@ -14,12 +14,16 @@ const renderMovies = () => {
   }
   movieList.innerHTML = '';
 
-  movies.forEach((movie) => {
+  const filteredMovies = !filter 
+  ? movies 
+  : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
     // movieEl.textContent = movie.info.title;
     let text = movie.info.title + ' - ';
     for (const key in movie.info){
-      if (key !== 'tittle'){
+      if (key !== 'title'){ // todos los que no sean title.. que despliegan? 
         text = text + `${key}: ${movie.info[key]}`; // This print Title I DON'T KNOW WHY! 
       }
     }
@@ -52,5 +56,11 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+}
+
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
