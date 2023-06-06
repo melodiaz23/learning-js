21996a147db539af8b5fd5b18985754e0b385316
melodiaz23 <meli.diaz23@gmail.com>
2023-06-06 10:00:17 -0500

    multiple decorators

--- typescript/4-decorators-01-starting-setup/src/app.ts
+++ typescript/4-decorators-01-starting-setup/src/app.ts
@@ -1,13 +1,27 @@
 function Logger(logString: string){// DECORATOR FACTORIES FUNCTION
+  console.log('logger factory')
   return function(constructor: Function){
   console.log(logString);
   console.log(constructor);
   };
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string){
+  console.log('template factory')
+  return function(constructor: any){
+    console.log('rendering template')
+    const hookEl = document.getElementById(hookId)!;
+    const p = new constructor();
+    hookEl.innerHTML = template;
+    hookEl.querySelector('h1')!.textContent = p.name;
+  }
+}
+
+// @Logger('LOGGING - PERSON')
+@Logger('LOGGING')
+@WithTemplate('<h1>My person object</h1>', 'app')
 class Person {
-  name = 'Max';
+  name = 'Mel';
 
   constructor(){
     console.log('creating person object...');
